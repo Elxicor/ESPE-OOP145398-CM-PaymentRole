d5890187cd06b97b9708afc05f3840c4b0e67f5e
Erick_Tufiño <eato2001@hotmail.com>
2024-06-20 21:03:56 -0500

    register user

--- Code/RolePaymentSystem2.0/src/ec/edu/espe/rolepaymentsystem/view/MainWindow.java
+++ Code/RolePaymentSystem2.0/src/ec/edu/espe/rolepaymentsystem/view/MainWindow.java
@@ -6,6 +6,7 @@ package ec.edu.espe.rolepaymentsystem.view;
 import ec.edu.espe.rolepaymentsystem.controller.EmployeeManager;
 import ec.edu.espe.rolepaymentsystem.controller.LoginScreen;
 import ec.edu.espe.rolepaymentsystem.controller.PayrollGenerator;
+import java.util.Scanner;
 
 /**
  *
@@ -13,12 +14,41 @@ import ec.edu.espe.rolepaymentsystem.controller.PayrollGenerator;
  */
 public class MainWindow {
     public static void main(String[] args) {
-            LoginScreen loginScreen = new LoginScreen();
-            loginScreen.checkPassword();
-            EmployeeManager employeeManager = new EmployeeManager();
-            PayrollGenerator payrollGenerator = new PayrollGenerator();
-            UserInterface userInterface = new UserInterface(employeeManager, payrollGenerator);
-            userInterface.showMainMenu();
+        LoginScreen loginScreen = new LoginScreen();
+        Scanner scanner = new Scanner(System.in);
+
+        boolean exit = false;
+
+        while (!exit) {
+            try {
+                System.out.println("¿Desea registrar un nuevo usuario? (s/n) o presione 0 para salir: ");
+                String response = scanner.nextLine();
+
+                if (response.equals("0")) {
+                    exit = true;
+                    continue;
+                }
+
+                if (response.equalsIgnoreCase("s")) {
+                    loginScreen.registerUser();
+                }
+
+                if (loginScreen.checkPassword()) {
+                    EmployeeManager employeeManager = new EmployeeManager();
+                    PayrollGenerator payrollGenerator = new PayrollGenerator();
+                    UserInterface userInterface = new UserInterface(employeeManager, payrollGenerator);
+                    userInterface.showMainMenu();
+                    exit = true; // Salir del bucle después de iniciar sesión con éxito
+                } else {
+                    System.out.println("Credenciales incorrectas. Intente nuevamente.");
+                }
+            } catch (Exception e) {
+                System.out.println("Se produjo un error: " + e.getMessage());
+                System.out.println("Intente nuevamente.");
+            }
+        }
+
+        System.out.println("Saliendo del sistema...");
     }
 }
  
